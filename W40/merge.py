d4e515add1fe228a383e5c9b2e2072650debb419
Randi Dannevang Fehrn <dannevang21@outlook.dk>
2020-10-01 20:06:13 +0200

    merges

--- W40/merge.py
+++ W40/merge.py
@@ -10,8 +10,30 @@ def merge(xs, ys):
     >>> merge([1,3], [2,4])
     [1, 2, 3, 4]
     """
-
-
+    # recursiv
+    if not xs:
+        return ys
+    if not ys:
+        return xs
+    x, *xs_ = xs
+    y, *ys_ = ys
+    if x < y: return [x] + merge(xs_, ys)
+    else: return [y] + merge(xs, ys_)
+    
+    # iterative
+    
+    i = 0
+    j = 0
+    while True.
+        if j > len(ys) or xs[i] < ys[j]:
+        
+            merged.append(xs[i])
+            i += 1
+        elif j < len(ys):
+            merged.append(ys[j])
+            j += 1
+        else:
+            break
 
 
 if __name__ == "__main__":
