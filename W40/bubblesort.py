475b4a42a605eb89ec495b6c05bb15f774be6c88
Randi Dannevang Fehrn <dannevang21@outlook.dk>
2020-10-01 19:36:51 +0200

    løsning

--- W40/bubblesort.py
+++ W40/bubblesort.py
@@ -5,13 +5,35 @@ def bubblesort(lst):
 
     >>> bubblesort([5, 2, 3])
     [2, 3, 5]
+    >>> bubblesort([15, 10, 9])
+    [9, 10, 15]
     >>> bubblesort([])
     []
     >>> bubblesort([1])
     [1]
     """
-
+    n = len(lst)
+    
+    lst = list(lst)
+    
+    if len(lst) <= 1:
+        return lst
+    
+    changes = True
+    
+    while changes:
+        changes = False
+        
+        for i in range(n-1):
+            for j in range(0, n-i-1):
+            
+                if lst[j] > lst[j+1]:
+                    lst[j], lst[j+1] = lst[j+1], lst[j]
+                    change = True
+    return lst
+        
 
 if __name__ == "__main__":
     import doctest
-    doctest.testmod()
+    if doctest.testmod().failed == 0:
+        print("Den er færdig")
